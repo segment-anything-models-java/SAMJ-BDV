7fc0ab2d15296fb55447184db08455c80edcfd1b
Vladimír Ulman <ulman@fi.muni.cz>
2024-07-17 11:56:34 +0200

    FIX: polishing the integration of the main SAMJ dialog window

--- src/main/java/ai/nets/samj/bdv/SAMJ_BDV_Annotator.java
+++ src/main/java/ai/nets/samj/bdv/SAMJ_BDV_Annotator.java
@@ -22,16 +22,19 @@ package ai.nets.samj.bdv;
 import ai.nets.samj.bdv.ui.BDVPromptsProvider;
 import ai.nets.samj.communication.model.SAMModels;
 import ai.nets.samj.gui.SAMJDialog;
+import ai.nets.samj.gui.components.ComboBoxItem;
 import ai.nets.samj.ui.SAMJLogger;
-import ai.nets.samj.bdv.ui.IJSamMethods;
 import net.imagej.ImgPlus;
+import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.img.Img;
+import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 import sc.fiji.simplifiedio.SimplifiedIO;
 
 import javax.swing.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.Arrays;
 
 /**
  * @author Vladimir Ulman
@@ -39,7 +42,7 @@ import java.awt.event.WindowEvent;
 public class SAMJ_BDV_Annotator {
 
 	//TODO: maybe RAI would be enough
-	public <T extends RealType<T>> void startBdvAnnotation(final Img<T> imageToBeAnnotated) {
+	public <T extends RealType<T> & NativeType<T>> void startBdvAnnotation(final Img<T> imageToBeAnnotated) {
 		try {
 			// TODO I (Carlos) don't know how to develop in IJ2 Logger guiSublogger = log.subLogger("PromptsResults window");
 			SAMJLogger guilogger = new SAMJLogger() {
@@ -68,13 +71,20 @@ public class SAMJ_BDV_Annotator {
 				public void error(String text) {System.out.println("BDV -- " + text);}
 			};
 
-			//get list of recognized installations of SAM(s)
-			final SAMModels availableModels = new SAMModels();
 
 			SwingUtilities.invokeLater(() -> {
-						  //TODO: IJSamMethods() will be listing just the file I was given.. in fact it would show a placeholder text
-						  //TODO: then remove the src code from this repo
-						  SAMJDialog samjDialog = new SAMJDialog(availableModels, new IJSamMethods(), guilogger, networkLogger);
+						  SAMJDialog samjDialog = new SAMJDialog(
+								    //get list of recognized installations of SAM(s)
+								    new SAMModels(),
+									 //get list of available images (which would be only the one given here!)
+								    () -> Arrays.asList(new ComboBoxItem(99, imageToBeAnnotated ) {
+									    @Override
+									    public String getImageName() { return "N/A, Currently working only with the image inside BDV"; }
+									    @Override
+									    public RandomAccessibleInterval<T> getImageAsImgLib2() { return imageToBeAnnotated; }
+								    }),
+									 //loggers....
+								    guilogger, networkLogger );
 						  //create the GUI (BDV on the given image) adapter between the user inputs/prompts and SAMJ outputs
 						  samjDialog.setPromptsProvider((imgAsObject) -> new BDVPromptsProvider(imageToBeAnnotated, bdvLogger));
 

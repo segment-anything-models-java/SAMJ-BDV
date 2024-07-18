6254c5b90ab1b88b4276bffb9151092216ccf3a7
Vladimír Ulman <ulman@fi.muni.cz>
2024-07-18 15:08:51 +0200

    ADD: NamedSpatioTemporalView - a layer above that can hold its name too

---
+++ src/main/java/ai/nets/samj/bdv/util/NamedSpatioTemporalView.java
@@ -0,0 +1,16 @@
+package ai.nets.samj.bdv.util;
+
+import bdv.util.BdvHandle;
+
+public class NamedSpatioTemporalView extends SpatioTemporalView {
+	public NamedSpatioTemporalView(BdvHandle bdv, String name) {
+		super(bdv);
+		this.name = name;
+	}
+
+	private final String name;
+
+	public String getName() {
+		return name;
+	}
+}

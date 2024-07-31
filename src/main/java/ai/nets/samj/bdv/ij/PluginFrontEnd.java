87c94add99ea34ffe145492b1ccaac3c0be2fad1
Vladimír Ulman <ulman@fi.muni.cz>
2024-07-31 16:26:20 +0200

    ADD2/2: plugin front end demos writing to original image where SAMJ polygons should be

--- src/main/java/ai/nets/samj/bdv/ij/PluginFrontEnd.java
+++ src/main/java/ai/nets/samj/bdv/ij/PluginFrontEnd.java
@@ -2,6 +2,7 @@ package ai.nets.samj.bdv.ij;
 
 import ai.nets.samj.bdv.SAMJ_BDV_Annotator;
 import net.imagej.Dataset;
+import net.imglib2.RandomAccess;
 import net.imglib2.img.Img;
 import net.imglib2.type.numeric.RealType;
 import org.scijava.command.Command;
@@ -19,6 +20,9 @@ public class PluginFrontEnd implements Command {
 	@Parameter
 	boolean useFakePromptResults = false;
 
+	@Parameter
+	boolean drawSmallXYcrossesIntoPolygonsCentres = false;
+
 	@Override
 	public void run() {
 		SAMJ_BDV_Annotator annotator = new SAMJ_BDV_Annotator();
@@ -28,5 +32,46 @@ public class PluginFrontEnd implements Command {
 		annotator.startBdvAnnotation((Img)origImage, inputImage.getName());
 		annotator.setShowSubmittedImagesToSAM( showImagesSubmittedToSAM );
 		annotator.setReturnFakeSAMResults( useFakePromptResults );
+
+		if (drawSmallXYcrossesIntoPolygonsCentres) {
+			annotator.registerPolygonsConsumer(p -> {
+				double[] centre = new double[3];
+				for (int i = 0; i < p.size(); ++i) {
+					double[] c = p.coordinate3D(i);
+					centre[0] += c[0];
+					centre[1] += c[1];
+					centre[2] += c[2];
+				}
+				if (p.size() > 0) {
+					System.out.println("Hey, gonna draw a black xy-cross into polygon's centre in the original image.");
+					centre[0] /= p.size();
+					centre[1] /= p.size();
+					centre[2] /= p.size();
+
+					RandomAccess<? extends RealType<?>> ra = origImage.randomAccess();
+					ra.setPosition(Math.round(centre[0]), 0);
+					ra.setPosition(Math.round(centre[1]), 1);
+					ra.setPosition(Math.round(centre[2]), 2);
+
+					ra.get().setZero();
+
+					ra.fwd(0);
+					ra.get().setZero();
+					ra.bck(0);
+					ra.bck(0);
+					ra.get().setZero();
+					ra.fwd(0);
+
+					ra.fwd(1);
+					ra.get().setZero();
+					ra.bck(1);
+					ra.bck(1);
+					ra.get().setZero();
+					//ra.fwd(1);
+
+					inputImage.update();
+				}
+			});
+		}
 	}
 }

81290018e7e65a2854400570d631c48bb8496675
Vladimír Ulman <ulman@fi.muni.cz>
2024-07-31 15:19:17 +0200

    FIX,COS: demo Polygons3DExampleConsumer prints local 2D coords as integers (pixel coords)

--- src/main/java/ai/nets/samj/bdv/polygons/Polygons3DExampleConsumer.java
+++ src/main/java/ai/nets/samj/bdv/polygons/Polygons3DExampleConsumer.java
@@ -17,7 +17,7 @@ public class Polygons3DExampleConsumer implements Consumer<Polygon3D> {
 		sb = new StringBuilder("Got 3D poly: [ ");
 		for (int i = 0; i < polygon.size(); ++i) {
 			double[] c = polygon.coordinate2D(i);
-			sb.append("["+c[0]+","+c[1]+"]");
+			sb.append("["+Math.round(c[0])+","+Math.round(c[1])+"]");
 			if (i < polygon.size()-1) sb.append(",");
 		}
 		sb.append(" ] - view (local) coordinates");

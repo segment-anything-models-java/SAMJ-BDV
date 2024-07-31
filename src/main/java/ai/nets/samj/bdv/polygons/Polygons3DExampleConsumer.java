323597f4e675f4b0a468a9dc6e1868f455fbae9c
Vladimír Ulman <ulman@fi.muni.cz>
2024-07-31 14:04:52 +0200

    ADD: Polygons3DExampleConsumer to demo (and print) the Polygon3D

---
+++ src/main/java/ai/nets/samj/bdv/polygons/Polygons3DExampleConsumer.java
@@ -0,0 +1,26 @@
+package ai.nets.samj.bdv.polygons;
+
+import java.util.function.Consumer;
+
+public class Polygons3DExampleConsumer implements Consumer<Polygon3D> {
+	@Override
+	public void accept(Polygon3D polygon) {
+		StringBuilder sb = new StringBuilder("Got 3D poly: [ ");
+		for (int i = 0; i < polygon.size(); ++i) {
+			double[] c = polygon.coordinate3D(i);
+			sb.append("["+c[0]+","+c[1]+","+c[2]+"]");
+			if (i < polygon.size()-1) sb.append(",");
+		}
+		sb.append(" ] - image grid (global) coordinates");
+		System.out.println(sb);
+
+		sb = new StringBuilder("Got 3D poly: [ ");
+		for (int i = 0; i < polygon.size(); ++i) {
+			double[] c = polygon.coordinate2D(i);
+			sb.append("["+c[0]+","+c[1]+"]");
+			if (i < polygon.size()-1) sb.append(",");
+		}
+		sb.append(" ] - view (local) coordinates");
+		System.out.println(sb);
+	}
+}

1665aee1498d82703e132bc3e9a358cde901dfd1
Vladimír Ulman <ulman@fi.muni.cz>
2024-11-01 21:25:37 +0100

    CHG4/4: Prompts reorganized to create:
    - default seeds creator by thresholding (+closing) that conforms
      to SeedsFromPromptCreator iface
    - converter from seeds to AABBs, which no longer sees the originalRAI
      and thus can be only filtering AABBs by their size and no longer
      also by their max intensity
    - added aux helper: createImgOfSameTypeAndSize()

--- src/main/java/ai/nets/samj/util/Prompts.java
+++ src/main/java/ai/nets/samj/util/Prompts.java
@@ -2,8 +2,6 @@ package ai.nets.samj.util;
 
 import bdv.tools.brightness.ConverterSetup;
 import net.imglib2.Cursor;
-import net.imglib2.IterableInterval;
-import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.algorithm.labeling.ConnectedComponents;
 import net.imglib2.algorithm.morphology.Closing;
@@ -11,17 +9,19 @@ import net.imglib2.algorithm.neighborhood.RectangleShape;
 import net.imglib2.algorithm.neighborhood.Shape;
 import net.imglib2.converter.Converters;
 import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgFactory;
 import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.loops.LoopBuilder;
+import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
-import net.imglib2.type.numeric.integer.UnsignedByteType;
+import net.imglib2.type.numeric.integer.ByteType;
 import net.imglib2.type.numeric.integer.UnsignedShortType;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.view.Views;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -37,10 +37,16 @@ public class Prompts {
 	private static int SHOW_DBGIMAGE_COUNTER = 0;
 	private static final Shape SE_FOR_CLOSING = new RectangleShape(1, false);
 
-	public static <T extends RealType<T>>
-	Img<UnsignedShortType> getSeedComponents(final RandomAccessibleInterval<T> originalRAI,
-	                                         final ConverterSetup contrastAdjustment,
-	                                         final int showDebugImagesFlag) {
+	public static <T extends RealType<T> & NativeType<T>>
+	Img<T> createImgOfSameTypeAndSize(final RandomAccessibleInterval<T> templateImg) {
+		T type = templateImg.getAt( templateImg.minAsLongArray() );
+		return new ArrayImgFactory<>(type).create(templateImg);
+	}
+
+	public static <T extends RealType<T> & NativeType<T>>
+	Img<T> getSeedsByContrastThresholdingAndClosing(final RandomAccessibleInterval<T> originalRAI,
+	                                                final ConverterSetup contrastAdjustment,
+	                                                final int showDebugImagesFlag) {
 		SHOW_DBGIMAGE_COUNTER++;
 		if ((showDebugImagesFlag & SHOW_ORIGINAL_DBGIMAGE) > 0) {
 			ImageJFunctions.show(originalRAI, SHOW_DBGIMAGE_COUNTER + ": source original image");
@@ -52,7 +58,7 @@ public class Prompts {
 		if (max == min) max += 1.0;
 		final double range = max - min;
 
-		final RandomAccessibleInterval<UnsignedByteType> thresholdedImg = ArrayImgs.unsignedBytes(originalRAI.dimensionsAsLongArray());
+		Img<T> thresholdedImg = createImgOfSameTypeAndSize(originalRAI);
 		LoopBuilder.setImages(originalRAI, thresholdedImg).forEachPixel( (i, o) -> {
 			double px = Math.min(Math.max(i.getRealDouble() - min, 0.0) / range, 1.0);
 			if (px > 0) o.setOne(); else o.setZero();
@@ -68,71 +74,65 @@ public class Prompts {
 			ImageJFunctions.show(thresholdedImg, SHOW_DBGIMAGE_COUNTER+": thresholded image");
 		}
 
-		final Img<UnsignedByteType> closedThesholdedImg = ArrayImgs.unsignedBytes(originalRAI.dimensionsAsLongArray());
+		final Img<T> closedThesholdedImg = thresholdedImg.copy();
 		//NB: close() requires that input and output image be different memory, which is why the closedThesholdedImg is created
 		Closing.close(Views.extendValue(thresholdedImg,0), closedThesholdedImg, SE_FOR_CLOSING, 4);
 		if ((showDebugImagesFlag & SHOW_CLOSED_DBGIMAGE) > 0) {
 			ImageJFunctions.show(closedThesholdedImg, SHOW_DBGIMAGE_COUNTER+": thresholded then closed image");
 		}
 
-		final Img<UnsignedShortType> ccaImg = ArrayImgs.unsignedShorts(originalRAI.dimensionsAsLongArray());
+		return closedThesholdedImg;
+	}
+
+
+	/**
+	 * @param seedsRAI A binary image (zero vs. non-zero values) for CCA, to establish bounding boxes
+	 * @param showDebugImagesFlag Possibly show debug images; sensitive only to {@link #SHOW_COMPONENTS_DBGIMAGE} and {@link #SHOW_PROMPTS_DBGIMAGE}
+	 * @return List of axis-aligned bounding boxes around CCA'ed seeds, a box is [x0,y0, x1,y1] in _local_ (0-based) coordinates
+	 * @param <T> pixel type of the image submitted to the prompts processors, see {@link bdv.interactive.prompts.BdvPrompts}
+	 */
+	public static <T extends RealType<T> & NativeType<T>>
+	List<int[]> returnSeedsAsBoxes(final RandomAccessibleInterval<T> seedsRAI,
+	                               final int showDebugImagesFlag) {
+		final Img<UnsignedShortType> ccaImg = ArrayImgs.unsignedShorts(seedsRAI.dimensionsAsLongArray());
 		ccaImg.forEach(UnsignedShortType::setZero);
-		ConnectedComponents.labelAllConnectedComponents(closedThesholdedImg, ccaImg, ConnectedComponents.StructuringElement.EIGHT_CONNECTED);
+
+		ConnectedComponents.labelAllConnectedComponents(
+			Converters.convert(seedsRAI, (i,o) -> o.setInteger(i.getRealDouble() > 0.0 ? 1 : 0), new ByteType()),
+			ccaImg,
+			ConnectedComponents.StructuringElement.EIGHT_CONNECTED);
+
 		if ((showDebugImagesFlag & SHOW_COMPONENTS_DBGIMAGE) > 0) {
 			ImageJFunctions.show(ccaImg, SHOW_DBGIMAGE_COUNTER+": thresholded then closed then labels image");
 		}
 
-		return ccaImg;
-	}
+		final Map<Integer, int[]> boxes = new HashMap<>(100);
+		//NB: minX,minY, maxX,maxY
 
-	public static <T extends RealType<T>>
-	List<int[]> findSeedsAndReturnAsBoxes(final RandomAccessibleInterval<T> originalRAI,
-	                                      final ConverterSetup contrastAdjustment,
-	                                      final int showDebugImagesFlag) {
-		final Img<UnsignedShortType> ccaImg
-				= getSeedComponents(originalRAI, contrastAdjustment, showDebugImagesFlag);
-
-		final Map<Integer, int[]> boxesAndStats = new HashMap<>(100);
-		//NB: minX,minY, maxX,maxY, maxIntensity
-
-		final RandomAccess<T> origPx = originalRAI.randomAccess();
 		final Cursor<UnsignedShortType> ccaPx = ccaImg.localizingCursor();
 		final int[] ccaPxPos = new int[2];
 		while (ccaPx.hasNext()) {
 			final int px = ccaPx.next().get();
 			if (px == 0) continue; //NB: skip background pixel
 			ccaPx.localize(ccaPxPos);
-			ccaPxPos[0] += originalRAI.min(0); //NB: brings it to the coord system of originalRAI
-			ccaPxPos[1] += originalRAI.min(1);
-			if (!boxesAndStats.containsKey(px)) boxesAndStats.put(px, new int[] {ccaPxPos[0],ccaPxPos[1],ccaPxPos[0],ccaPxPos[1],px});
-			int[] box = boxesAndStats.get(px);
+			if (!boxes.containsKey(px)) boxes.put(px, new int[] {ccaPxPos[0],ccaPxPos[1],ccaPxPos[0],ccaPxPos[1]});
+			int[] box = boxes.get(px);
 			box[0] = Math.min(box[0], ccaPxPos[0]);
 			box[1] = Math.min(box[1], ccaPxPos[1]);
 			box[2] = Math.max(box[2], ccaPxPos[0]);
 			box[3] = Math.max(box[3], ccaPxPos[1]);
-			origPx.setPosition(ccaPxPos);
-			box[4] = Math.max(box[4], (int)origPx.get().getRealDouble());
 		}
-
-		final double min = contrastAdjustment.getDisplayRangeMin();
-		double max = contrastAdjustment.getDisplayRangeMax();
-		if (max == min) max += 1.0;
-		final double range = max - min;
-		final int minimalBrightestIntensityThreshold = (int)(0.8*range + min);
-		System.out.println("Considering only components brighter than "+minimalBrightestIntensityThreshold);
 
 		final boolean doPromptsDebug = (showDebugImagesFlag & SHOW_PROMPTS_DBGIMAGE) > 0;
 		if (doPromptsDebug) ccaImg.forEach(UnsignedShortType::setZero);
 		//
-		List<int[]> seeds = new ArrayList<>(boxesAndStats.size());
-		for (int[] box : boxesAndStats.values()) {
+		List<int[]> seeds = new ArrayList<>(boxes.size());
+		for (int[] box : boxes.values()) {
 			if ((box[2]-box[0])*(box[3]-box[1]) < 25) continue;         //skip over very small patches
-			if (box[4] < minimalBrightestIntensityThreshold) continue;  //skip over non-bright patches
+			//if (box[4] < minimalBrightestIntensityThreshold) continue;  //skip over non-bright patches
 			seeds.add(box);
 			if (doPromptsDebug) {
-				Views.interval(ccaImg,
-						  new long[] {box[0]-originalRAI.min(0), box[1]-originalRAI.min(1)},
-						  new long[] {box[2]-originalRAI.min(0), box[3]-originalRAI.min(1)})
+				Views.interval(ccaImg, new long[] {box[0], box[1]}, new long[] {box[2], box[3]})
 						  .forEach(UnsignedShortType::setOne);
 			}
 		}

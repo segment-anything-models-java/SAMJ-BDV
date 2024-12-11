3ed93a9d8e17a96b65520fec94346dc8967443f2
Vladimír Ulman <ulman@fi.muni.cz>
2024-12-11 16:34:16 +0100

    FIX2/2: AbstractPlanarShapeWithImgIn3D has to use NativeType too,...
    ...because PromptsProcessor had to switch to NativeType

--- src/main/java/bdv/interactive/prompts/planarshapes/AbstractPlanarShapeWithImgIn3D.java
+++ src/main/java/bdv/interactive/prompts/planarshapes/AbstractPlanarShapeWithImgIn3D.java
@@ -1,10 +1,11 @@
 package bdv.interactive.prompts.planarshapes;
 
 import net.imglib2.img.Img;
+import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.realtransform.AffineTransform3D;
 
-public abstract class AbstractPlanarShapeWithImgIn3D <T extends RealType<T>> extends AbstractPlanarShapeIn3D {
+public abstract class AbstractPlanarShapeWithImgIn3D <T extends RealType<T> & NativeType<T>> extends AbstractPlanarShapeIn3D {
 	public AbstractPlanarShapeWithImgIn3D(final int expectedNoOfVertices, final Img<T> viewImage2D, final AffineTransform3D viewTo3dTransform) {
 		super(expectedNoOfVertices, viewTo3dTransform);
 		supportingViewImage2D = viewImage2D;

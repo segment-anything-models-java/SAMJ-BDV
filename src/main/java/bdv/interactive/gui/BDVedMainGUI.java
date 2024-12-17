a8ce48700082fa10afca8a155219115b22486331
Vladimír Ulman <ulman@fi.muni.cz>
2024-12-17 23:37:43 +0100

    ADD: my own (but not finished!) middle panel content (two cards)

--- src/main/java/bdv/interactive/gui/BDVedMainGUI.java
+++ src/main/java/bdv/interactive/gui/BDVedMainGUI.java
@@ -7,6 +7,8 @@ import ai.nets.samj.ui.ConsumerInterface;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
+import net.miginfocom.layout.CC;
+import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
 import java.awt.*;
@@ -41,17 +43,50 @@ public class BDVedMainGUI extends MainGUI {
 	}
 	protected JLabel associatedBdvLabelComponent;
 
+
+	@Override
+	protected JPanel createSecondComponent() {
+		JPanel origPanel = super.createSecondComponent();
+		cardPanel.remove(0);
+		cardPanel.remove(0);
+
+		JPanel card1 = new JPanel(new MigLayout("fill","center","[]push"));
+		JTextPane htmlText = new JTextPane();
+		htmlText.setContentType("text/html");
+		htmlText.setText(SAMJ_CONTROLS_HELP_HTML);
+		card1.add( new JScrollPane(htmlText,
+				  JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED) );
+
+		JPanel card2 = new JPanel(new MigLayout("fill","[c][c][c]"));
+		card2.add(new JTextField("some script path"), new CC().grow().span());
+		card2.add(new JButton("Template"), new CC().grow(1));
+		card2.add(new JButton("Browse"), new CC().grow(1));
+		card2.add(new JButton("Run..."), new CC().grow(2));
+
+		cardPanel.add(card1, MANUAL_STR);
+		cardPanel.add(card2, PRESET_STR);
+		cardPanel.setPreferredSize(new Dimension(MAIN_HORIZONTAL_SIZE-30, (int)(0.3*MAIN_VERTICAL_SIZE)));
+		return origPanel;
+	}
+
+
 	@Override
 	protected JPanel createThirdComponent() {
 		JPanel origPanel = super.createThirdComponent();
 		origPanel.remove(chkRoiManager);
 		return origPanel;
 	}
+	protected static int MAIN_HORIZONTAL_SIZE = 400;
 
 
 	protected void touchUpForBdv() {
 		retunLargest.setEnabled(true);
 		export.setEnabled(true);
+
+		radioButton1.setEnabled(true);
+		radioButton2.setEnabled(true);
+		cardPanel.setEnabled(true);
+
 		setSize(MAIN_HORIZONTAL_SIZE, MAIN_VERTICAL_SIZE - 50);
 
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
@@ -59,6 +94,13 @@ public class BDVedMainGUI extends MainGUI {
 		close.addActionListener( (ignore) -> hideWindow() );
 	}
 
+	public static final String SAMJ_CONTROLS_HELP_HTML =
+			"<html><b>Hold down L</b> key, <b>Left Click and Drag</b> and <b>Release L</b>" +
+			" on the image to select a region inside which SAMJ will annotate," +
+			" operating on the <b>original image</b>.<br/><br/>" +
+			"<b>Hold down K</b> key, <b>Left Click and Drag</b> and <b>Release K</b>" +
+			" on the image to select a region inside which SAMJ will annotate," +
+			" operating <b>under the current contrast</b> setting.</html>";
 
 	final static ConsumerInterface emptyFakeConsumer = new ConsumerInterface() {
 		@Override

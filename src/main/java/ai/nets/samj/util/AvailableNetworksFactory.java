5ccfc07347a12e00333acf62cf419c4455de125b
Vladimír Ulman <ulman@fi.muni.cz>
2025-04-17 17:38:20 +0200

    ADD: AvailableNetworksFactory.reportAndChooseFirstAvailable() util method

--- src/main/java/ai/nets/samj/util/AvailableNetworksFactory.java
+++ src/main/java/ai/nets/samj/util/AvailableNetworksFactory.java
@@ -1,10 +1,14 @@
 package ai.nets.samj.util;
 
+import ai.nets.samj.bdv.promptresponders.SamjResponder;
 import ai.nets.samj.communication.model.SAMModel;
 import ai.nets.samj.communication.model.SAMModels;
+import bdv.interactive.prompts.BdvPrompts;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class AvailableNetworksFactory {
 	private final List<String> netNames = new ArrayList<>(10);
@@ -32,5 +36,20 @@ public class AvailableNetworksFactory {
 			if (netNames.get(i).equals(ofThisNetName)) return netObjs.get(i);
 		}
 		return null;
+	}
+
+	public static SAMModel reportAndChooseFirstAvailable(final BdvPrompts<?,?> samj,
+	                                                     Consumer<String> consoleReporter) {
+		final AvailableNetworksFactory netFactory = new AvailableNetworksFactory();
+		final List<String> availableModels = netFactory.availableModels();
+		if (!availableModels.isEmpty()) {
+			//grab the first available network
+			SAMModel net = netFactory.getModel(availableModels.get(0));
+			samj.addPromptsProcessor(new SamjResponder<>(net));
+			availableModels.forEach(netName -> consoleReporter.accept("Detected available SAMJ: "+netName));
+			return net;
+		} else {
+			return null;
+		}
 	}
 }
